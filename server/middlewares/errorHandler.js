dea91b542e104f23e5b0b5f1d29f936d1146675c
naufalfikri0206 <Naufalfikri0206@gmail.com>
2024-03-19 13:04:23 +0700

    update

---
+++ server/middlewares/errorHandler.js
@@ -0,0 +1,45 @@
+const errorHandler = (error, req, res, next) => {
+  console.log(error);
+  let code = 500;
+  let message = "INTERNAL SERVER ERROR";
+
+  if (err.name === "LoginError") {
+    code = 401;
+    message = "Email and password are required";
+  }
+
+  if (error.name === "SequelizeDatabaseError") {
+    code = 400;
+    message = "Invalid input";
+  }
+  if (error.name === "JsonWebTokenError") {
+    code = 401;
+    message = "Invalid token";
+  }
+  if (error.name === "Invalid email/password") {
+    code = 401;
+    message = "Invalid email/password";
+  }
+  if (error.name == "Unauthorized") {
+    code = 401;
+    message = "Invalid token";
+  }
+  if (err.name === "EmailFormat") {
+    code = 401;
+    message = "Invalid email format";
+  }
+
+  if (err.name === "EmptyEmail") {
+    code = 401;
+    message = "Email is required";
+  }
+
+  if (err.name === "EmptyPassword") {
+    code = 401;
+    message = "Password is required";
+  }
+
+  res.status(code).json({ message });
+};
+
+module.exports = errorHandler;
